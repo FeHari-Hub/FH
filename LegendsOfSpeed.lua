a1f0192c5275c658a401e6b7a085110ea09b2a79
FeHari <alexandregomesap202@gmail.com>
2024-08-24 23:58:49 -0300

    LegendsOfSpeed.lua

--- LegendsOfSpeed.lua
+++ LegendsOfSpeed.lua
@@ -1,121 +1,32 @@
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHari/HaridadeScript/main/Connector.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x1KAITO/Legends-Of-Speed-/main/Connector.lua"))()
 
---WINDOW
+local KAITO = Instance.new("ScreenGui")
+local K = Instance.new("TextButton")
 
-local Window = Rayfield:CreateWindow({
-    Name = "Rayfield Example Window",
-    LoadingTitle = "Rayfield Interface Suite",
-    LoadingSubtitle = "by Sirius",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "Big Hub"
-    },
-    Discord = {
-       Enabled = false,
-       Invite = "uydz6pZWMk", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = true, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "KxnanTheDevs Script",
-       Subtitle = "Key System",
-       Note = "Join the discord)",
-       FileName = "SiriusKey",
-       SaveKey = true,
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = "RICK ASTLEY"
-    }
- })
+KAITO.Name = "KAITO"
+KAITO.Parent = game.CoreGui
+KAITO.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+K.Name = "K"
+K.Parent = KAITO
+K.BackgroundColor3 = Color3.fromRGB(129, 0, 0)
+K.Position = UDim2.new(0.128011018, 0, 0.100728154, 0)
+K.Size = UDim2.new(0, 70, 0, 58)
+K.Font = Enum.Font.Bodoni
+K.Text = "K"
+K.TextColor3 = Color3.fromRGB(255, 255, 255)
+K.TextSize = 72.000
+K.MouseButton1Click:Connect(function()
+game.CoreGui:FindFirstChild("ui").Enabled = not game.CoreGui:FindFirstChild("ui").Enabled
+wait(1)
+end)
 
 
- -- ANTI-CHEAT BYPASSED
+local function FBZDL_fake_script() -- K.LocalScript 
+	local script = Instance.new('LocalScript', K)
 
- Rayfield:Notify({
-    Title = "Anti-Cheat bypassed",
-    Content = "We have bypassed the Anti-Cheat",
-    Duration = 6.5,
-    Image = 4483362458,
-    Actions = { -- Notification Buttons
-       Ignore = {
-          Name = "Okay!",
-          Callback = function()
-          print("The user tapped Okay!")
-       end
-    },
- },
- })
-
-
- --TABS
-
- local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
-
- local GamePlayTab = Window:CreateTab("GamePlay", 4483362458) -- Title, Image
-
- 
- --FUNCTIONS
-
- local Slider = PlayerTab:CreateSlider({
-	Name = "WalkSpeed",
-	Range = {16, 10000},
-	Increment = 10,
-	Suffix = "WalkSpeed",
-	CurrentValue = 16,
-	Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(s)
-		game.Players.LocalPlayer.character.Humanoid.WalkSpeed = s
-	end,
-})
-
-local Slider = PlayerTab:CreateSlider({
-    Name = "JumpPower",
-    Range = {0, 5000},
-    Increment = 10,
-    Suffix = "JumpPower",
-    CurrentValue = 10,
-    Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(a)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-    end,
- })
-
-
- local Toggle = GamePlayTab:CreateToggle({
-    Name = "Toggle AutoSpeed/XP",
-    CurrentValue = false,
-    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-        while wait() do
-            local args = {
-                [1] = "collectOrb",
-                [2] = "Red Orb",
-                [3] = "Magma City"
-            }
-            
-            game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-            
-            
-    end,
- })
-
-
-
- local Toggle = GamePlayTab:CreateToggle({
-    Name = "Toggle AutoGem",
-    CurrentValue = false,
-    Flag = "Toggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-        while wait() do
-            local args = {
-                [1] = "collectOrb",
-                [2] = "Gem",
-                [3] = "Magma City"
-            }
-            
-            game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-            
-    end,
- })
-
-
+	script.Parent.Active = true
+	script.Parent.Selectable = true
+	script.Parent.Draggable = true
+end
+coroutine.wrap(FBZDL_fake_script)()
