b914150ae01a89b430c06f1218ce853b061d0467
HA_FeHari <alexandregomesap202@gmail.com>
2024-08-25 02:02:11 -0300

    Connector.lua

--- Connector.lua
+++ Connector.lua
@@ -1,217 +1,121 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
-local Window = OrionLib:MakeWindow({Name = "Haridade Script", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+--WINDOW
 
-local Tab = Window:MakeTab({
-	Name = "Auto Farm",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local Window = Rayfield:CreateWindow({
+    Name = "Rayfield Example Window",
+    LoadingTitle = "Rayfield Interface Suite",
+    LoadingSubtitle = "by Sirius",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "SIRIUS", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+    KeySystem = true, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "KxnanTheDevs Script",
+       Subtitle = "Key System",
+       Note = "Join the discord)",
+       FileName = "SiriusKey",
+       SaveKey = true,
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = "RICK ASTLEY"
+    }
+ })
+
+
+ -- ANTI-CHEAT BYPASSED
+
+ Rayfield:Notify({
+    Title = "Anti-Cheat bypassed",
+    Content = "We have bypassed the Anti-Cheat",
+    Duration = 6.5,
+    Image = 4483362458,
+    Actions = { -- Notification Buttons
+       Ignore = {
+          Name = "Okay!",
+          Callback = function()
+          print("The user tapped Okay!")
+       end
+    },
+ },
+ })
+
+
+ --TABS
+
+ local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
+
+ local GamePlayTab = Window:CreateTab("GamePlay", 4483362458) -- Title, Image
+
+ 
+ --FUNCTIONS
+
+ local Slider = PlayerTab:CreateSlider({
+	Name = "WalkSpeed",
+	Range = {16, 10000},
+	Increment = 10,
+	Suffix = "WalkSpeed",
+	CurrentValue = 16,
+	Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(s)
+		game.Players.LocalPlayer.character.Humanoid.WalkSpeed = s
+	end,
 })
 
-local Section = Tab:AddSection({
-	Name = "City"
-})
+local Slider = PlayerTab:CreateSlider({
+    Name = "JumpPower",
+    Range = {0, 5000},
+    Increment = 10,
+    Suffix = "JumpPower",
+    CurrentValue = 10,
+    Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(a)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end,
+ })
 
-Tab:AddToggle({
-	Name = "Auto Farm Blue Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmBlue = Value
-while AutoFarmBlue do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Blue Orb",
-    [3] = "City"
-}
 
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
+ local Toggle = GamePlayTab:CreateToggle({
+    Name = "Toggle AutoSpeed/XP",
+    CurrentValue = false,
+    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        while wait() do
+            local args = {
+                [1] = "collectOrb",
+                [2] = "Red Orb",
+                [3] = "Magma City"
+            }
+            
+            game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+            
+            
+    end,
+ })
 
-Tab:AddToggle({
-	Name = "Auto Farm Yellow Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmYellow = Value
-while AutoFarmYellow do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Gem",
-    [3] = "City"
-}
 
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
 
-Tab:AddToggle({
-	Name = "Auto Farm Orange Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmOrange = Value
-while AutoFarmOrange do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Orange Orb",
-    [3] = "City"
-}
+ local Toggle = GamePlayTab:CreateToggle({
+    Name = "Toggle AutoGem",
+    CurrentValue = false,
+    Flag = "Toggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        while wait() do
+            local args = {
+                [1] = "collectOrb",
+                [2] = "Gem",
+                [3] = "Magma City"
+            }
+            
+            game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+            
+    end,
+ })
 
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
 
-Tab:AddToggle({
-	Name = "Auto Farm Red Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmRed = Value
-while AutoFarmRed do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Red Orb",
-    [3] = "City"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Farm Gem",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmGem = Value
-while AutoFarmGem do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Gem",
-    [3] = "City"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
-
-local Section = Tab:AddSection({
-	Name = "Snowcity"
-})
-
-Tab:AddToggle({
-	Name = "Auto Farm Blue Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmBlue2 = Value
-while AutoFarmBlue2 do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Blue Orb",
-    [3] = "Snow city"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Farm Yellow Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmYellow2 = Value
-while AutoFarmYellow2 do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Yellow Orb",
-    [3] = "Snow city"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Farm Orange Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmOrange2 = Value
-while AutoFarmOrange2 do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Orange Orb",
-    [3] = "Snow city"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Farm Red Orbs",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmRed2 = Value
-while AutoFarmRed2 do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Red Orb",
-    [3] = "Snow city"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Farm Gem",
-	Default = false,
-	Callback = function(Value)
-		AutoFarmGem2 = Value
-while AutoFarmGem2 do
-for i = 1, 100 do
-local args = {
-    [1] = "collectOrb",
-    [2] = "Gem",
-    [3] = "Snow city"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
-end
-task.wait()
-end
-	end    
-})
