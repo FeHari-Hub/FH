38ee7d124657bc5201ad932d8a41d09d4308885d
HA_FeHari <alexandregomesap202@gmail.com>
2024-09-29 13:03:38 -0300

    Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,19 +1,3 @@
---> Jogos
-local games = {
-  [3101667897] = "https://raw.githubusercontent.com/FeHari/HaridadeScript/main/HATrial.lua"
-}
-
-local gameUrl = games[game.PlaceId]
-
---> Jogo Não Suportado
-if (not gameUrl) then
-  return game:GetService("Players").LocalPlayer:Kick("Oops! Este jogo ainda não é suportado. Estamos trabalhando para adicionar mais!)
-end
-
---> Carregar Script!
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHari/HaridadeScript/main/HATrial.lua"))()
-
-
   -- Jogos
 local GameID = game.PlaceId
 
