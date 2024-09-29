1c1f2bd29ac04e28d78d1dbfbe69315e1887b5af
HA_FeHari <alexandregomesap202@gmail.com>
2024-09-29 13:03:22 -0300

    Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -12,3 +12,24 @@ end
 
 --> Carregar Script!
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHari/HaridadeScript/main/HATrial.lua"))()
+
+
+  -- Jogos
+local GameID = game.PlaceId
+
+-- Table
+local Supported_Games = {
+    LegendsOfSpeed_City = 3101667897,
+    LegendsOfSpeed_Space = 3232996272,
+    LegendsOfSpeed_Desert = 3276265788,
+}
+
+-- Script
+if GameID == Supported_Games.LegendsOfSpeed_City
+or GameID == Supported_Games.LegendsOfSpeed_Space 
+or GameID == Supported_Games.LegendsOfSpeed_Desert then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/FeHari/HaridadeScript/main/HATrial.lua'),true))()
+elseif GameID == Supported_Games.MuscleLegends then
+else
+    warn("Oops! Este jogo ainda não é suportado. Estamos trabalhando para adicionar mais!")
+end
